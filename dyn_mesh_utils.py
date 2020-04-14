5761965bdbb8969bc2c5d01d7c23b65b06c01b54
wiSHFul97 <61869143+wiSHFul97@users.noreply.github.com>
2020-04-15 01:10:44 +0430

    first_commit
    developing wire floating mesh

---
+++ dyn_mesh_utils.py
@@ -0,0 +1,132 @@
+
+import bpy
+import bmesh
+from mathutils import Vector
+
+def other_vertex(vert, edge_index):
+    return vert.link_edges[edge_index].other_vert(vert)
+
+def get_edge_direction(from_vert, to_vert):
+    prev_vert = from_vert
+    for edge in from_vert.link_edges:
+        current_vert = edge.other_vert(from_vert)
+        while current_vert != None:
+            next_vert = get_next_wire_vert(prev_vert, current_vert)
+            prev_vert, current_vert = current_vert, next_vert
+            if current_vert == to_vert:
+                return edge
+    return None
+
+def generate_next_verts(prev_vert, current_vert, verts_count):
+    for i in range(verts_count):
+        next_vert = get_next_wire_vert(prev_vert, current_vert)
+        yield current_vert, next_vert, i
+        prev_vert, current_vert = current_vert, next_vert
+
+def generate_in_between_verts(from_vert, to_vert):
+    # should be wire (for now)
+    prev_vert = from_vert
+    edge = get_edge_direction(from_vert, to_vert)
+    current_vert = edge.other_vert(from_vert)
+    while current_vert != to_vert:
+        yield current_vert
+        next_vert = get_next_wire_vert(prev_vert, current_vert)
+        prev_vert, current_vert = current_vert, next_vert
+
+def add_additional_verts(last_vert, second_last_vert, additional_vert_count, wire, b_wire):
+    # select last edge --> subdivide --> move additional verts to last vert
+    bpy.ops.mesh.select_all(action='DESELECT')
+    second_last_vert.link_edges[0].select_set(True) # todo why second_last? should be last
+    last_vert_co = second_last_vert.co
+    bpy.ops.mesh.subdivide(number_cuts=additional_vert_count)
+    b_wire.verts.ensure_lookup_table()
+    added_verts = [v for v in b_wire.verts if v.select and len(v.link_edges)>1 and v.link_edges[0].select == v.link_edges[1].select]
+    for added_vert in added_verts:
+        added_vert.co = last_vert_co
+    prev_sec_last = [v for v in b_wire.verts if len(v.link_edges)>1 and v.link_edges[0].select != v.link_edges[1].select][0]
+    first_added_vert = other_vertex(prev_sec_last, 0) if prev_sec_last.link_edges[0].select else other_vertex(prev_sec_last, 1)
+    return prev_sec_last, first_added_vert
+    
+def get_next_wire_vert(prev_vert, current_vert):
+    if current_vert is None or len(current_vert.link_edges) <= 1:
+        return None # todo test
+    elif current_vert.link_edges[0].other_vert(current_vert) == prev_vert:
+        return current_vert.link_edges[1].other_vert(current_vert)
+    return current_vert.link_edges[0].other_vert(current_vert)
+
+def sum_in_between_vectors(move_percentage, line_vectors, last_vec_ind):
+    move_vector = Vector()
+    sign = (-1 if move_percentage < 0 else 1)
+    for j in range(int(abs(move_percentage))):
+        counter = j * sign
+        move_vector += line_vectors[last_vec_ind-counter]
+    move_vector += line_vectors[-int(move_percentage) + last_vec_ind] * (abs(move_percentage) % 1)
+    return move_vector * sign
+
+def slide_vert(current_vert, move_percentage, line_vectors, last_vec_ind):
+    if abs(move_percentage) > 1:
+        # find correct target vertex
+        move_vector = sum_in_between_vectors(move_percentage, line_vectors, last_vec_ind)
+        current_vert.co += move_vector
+        return 0
+    else:
+        current_vert.co += line_vectors[last_vec_ind] * move_percentage
+        del line_vectors[:last_vec_ind]
+        return last_vec_ind
+
+def slide_verts(verts_count, prev_vert, current_vert, move_constant, last_vec_ind):
+    line_vectors = [prev_vert.co - current_vert.co]
+    for cur_vert, next_vert, i in generate_next_verts(prev_vert, current_vert, verts_count):
+        move_percentage = move_constant * (i + 1)
+        line_vectors.append(cur_vert.co - next_vert.co)
+        slide_vert(cur_vert, move_percentage, line_vectors, last_vec_ind)
+
+    return move_percentage, cur_vert, next_vert, line_vectors
+
+def slide_last_verts(verts_count, prev_vert, current_vert, move_constant, move_percentage, last_vec_ind, line_vectors):
+    for current_vert, _, _ in generate_next_verts(prev_vert, current_vert, verts_count):
+        # next_vert = get_next_wire_vert(prev_vert, current_vert)
+        slide_vert(current_vert, move_percentage, line_vectors, last_vec_ind)
+        move_percentage -= (1 - move_constant)
+        # prev_vert, current_vert = current_vert, next_vert
+
+    return move_percentage, current_vert, prev_vert, line_vectors
+
+def get_line_vectors(segment_count, prev_vert, current_vert):
+    line_vector = [prev_vert.co - current_vert.co]
+    # for i in range(segment_count-1):
+    for current_vert, next_vert, _ in generate_next_verts(prev_vert, current_vert, segment_count-1):
+        # next_vert = get_next_wire_vert(prev_vert, current_vert)
+        line_vector.append(current_vert.co - next_vert.co)
+        # prev_vert, current_vert = current_vert, next_vert
+    return line_vector
+
+def dissolve_last_verts(index, line_vectors, prev_vert, current_vert):
+    bpy.ops.mesh.select_all(action='DESELECT')
+    # for j in range(index, len(line_vectors)):
+    for current_vert, next_vert, _ in generate_next_verts(prev_vert, current_vert, len(line_vectors)-index):    
+        current_vert.select = True
+        # next_vert = get_next_wire_vert(prev_vert, current_vert)
+        # prev_vert, current_vert = current_vert, next_vert
+    bpy.ops.mesh.dissolve_mode(use_verts=True)
+
+def out_slide_verts(line_vectors, prev_vert, current_vert, move_constant):
+    index = 0
+    for current_vert, next_vert, i in generate_next_verts(prev_vert, current_vert, len(line_vectors)-index):
+    # for i in range(len(line_vectors)-1):
+        # next_vert = get_next_wire_vert(prev_vert, current_vert)
+        move_percentage = move_constant * (i + 1)
+        index += 1
+        try:
+            index_offset = slide_vert(current_vert, move_percentage, line_vectors, index)
+            index -= index_offset
+        except:
+            # dissolve verts till the end...
+            dissolve_last_verts(index, line_vectors, prev_vert, current_vert)
+            break
+        # prev_vert, current_vert = current_vert, next_vert
+
+
+# todo next: add_additional_verts
+# refactor to use generator
+
---
+++ float_subdiv.py
@@ -0,0 +1,77 @@
+import bpy
+import bmesh
+from mathutils import Vector
+import math
+import os, sys
+directory = os.path.dirname(bpy.data.filepath)
+if not directory in sys.path:
+    sys.path.append(directory)
+from importlib import reload
+from dyn_mesh_utils import *
+reload(sys.modules['dyn_mesh_utils'])
+
+# todo think: har vaght move_percentage > 1 beshe dige az oonja be ba'd > 1 hast...
+# todo bug: if subdiv_lvl == 1: ye vert ezafe mishe alaki ...
+         
+class MESH_OT_float_subdiv(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "mesh.subdiv_float"
+    bl_label = "Float Subdivide"
+    bl_options = {'REGISTER', 'UNDO'}
+    subdiv_lvl: bpy.props.FloatProperty(
+        name='subdiv_lvl',
+        description='float subdivision level',
+        default=1.2,
+    )
+    
+    @classmethod
+    def poll(cls, context):
+        return context.area.type == 'VIEW_3D'
+
+    def execute(self, context):
+        # initialize
+        wire = context.active_object
+        b_wire = bmesh.from_edit_mesh(wire.data)
+        origin = [v for v in b_wire.verts if v.select][0]
+        prev_vert = origin
+        current_vert = prev_vert.link_edges[0].other_vert(prev_vert)
+        move_constant = (1 - 1/self.subdiv_lvl)
+        print(move_constant)
+        if move_constant > 0:
+            # slide existing verts
+            move_percentage, last_vert, second_last_vert, line_vectors = slide_verts(len(b_wire.verts)-2,
+                                                                    prev_vert, current_vert, move_constant, -2)
+            
+            # last vert and additional verts:
+            # last_vert.select = True 
+            # second_last_vert.select = True
+            move_percentage += move_constant
+            additional_vert_count = math.ceil(move_percentage/(1-move_constant))
+            last_vert, second_last_vert = add_additional_verts(last_vert, second_last_vert, additional_vert_count, wire, b_wire)
+            bmesh.update_edit_mesh(wire.data)
+
+            # sliding additional verts into proper position
+            slide_last_verts(additional_vert_count, second_last_vert, last_vert,
+                                move_constant, move_percentage, -1, line_vectors)        
+        else:
+            line_vectors = get_line_vectors(len(b_wire.verts)-1, prev_vert, current_vert)
+            out_slide_verts(line_vectors, prev_vert, current_vert, move_constant)
+
+        # revert selection
+        # bpy.ops.mesh.select_all(action='DESELECT')
+        # origin.select = True
+        # bmesh.update_edit_mesh(wire.data)
+        return {'FINISHED'}
+
+
+def register():
+    bpy.utils.register_class(MESH_OT_float_subdiv)
+
+
+def unregister():
+    bpy.utils.unregister_class(MESH_OT_float_subdiv)
+
+
+if __name__ == "__main__":
+    register()
+
